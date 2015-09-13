17e225fa40a746dd8155fa6fdf83f7628072ee40
fat2301 <fat2301@yahoo.com>
2015-09-14 01:02:06 +0800

    Signed-off-by: fat2301 <fat2301@yahoo.com>

---
+++ plot1.R
@@ -0,0 +1,25 @@
+library(data.table)
+
+colClasses<-c("Date", "character", "character", "character", "character", "character", "character", "character", "numeric")
+
+
+mydata<-fread("household_power_consumption.txt",header=TRUE,verbose=TRUE, sep=';', na.strings=c("?","","null","NA",'x',"","NULL"), 
+                 colClasses=colClasses)
+
+
+mydata2<- mydata[mydata$Date == '1/2/2007'| mydata$Date== '2/2/2007']
+
+mydata4<- mydata2[,dt:=as.POSIXct(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))]
+
+
+mydata5<- mydata4[,Global_active_power:=as.numeric(Global_active_power)]
+mydata6<- mydata5[,Global_reactive_power:=as.numeric(Global_reactive_power)]
+mydata7<- mydata6[,Voltage:=as.numeric(Voltage)]
+mydata8<- mydata7[,Global_intensity:=as.numeric(Global_intensity)]
+mydata9<- mydata8[,Sub_metering_1:=as.numeric(Sub_metering_1)]
+mydata10<- mydata9[,Sub_metering_2:=as.numeric(Sub_metering_2)]
+
+
+png(filename="plot1.png",widt=480, height=480, units="px")
+hist(mydata10$Global_active_power, col="red",xlab="Global Active Power (kilowatts)", main="Global Active Power")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,33 @@
+library(data.table)
+
+colClasses<-c("Date", "character", "character", "character", "character", "character", "character", "character", "numeric")
+
+
+mydata<-fread("household_power_consumption.txt",header=TRUE,verbose=TRUE, sep=';', na.strings=c("?","","null","NA",'x',"","NULL"), 
+                 colClasses=colClasses)
+
+
+mydata2<- mydata[mydata$Date == '1/2/2007'| mydata$Date== '2/2/2007']
+
+mydata4<- mydata2[,dt:=as.POSIXct(strptime(paste(Date,Time),"%d/%m/%Y %H:%M:%S"))]
+
+
+mydata5<- mydata4[,Global_active_power:=as.numeric(Global_active_power)]
+mydata6<- mydata5[,Global_reactive_power:=as.numeric(Global_reactive_power)]
+mydata7<- mydata6[,Voltage:=as.numeric(Voltage)]
+mydata8<- mydata7[,Global_intensity:=as.numeric(Global_intensity)]
+mydata9<- mydata8[,Sub_metering_1:=as.numeric(Sub_metering_1)]
+mydata10<- mydata9[,Sub_metering_2:=as.numeric(Sub_metering_2)]
+
+
+
+png(filename="plot3.png",widt=480, height=480, units="px")
+with(mydata10, plot(dt,Sub_metering_1,type="n", ylab="Energy sub metering",xlab=""))
+points(mydata10$dt,mydata10$Sub_metering_1,col="black",type='l')
+points(mydata10$dt,mydata10$Sub_metering_2,col="red",type='l')
+points(mydata10$dt,mydata10$Sub_metering_3,col="blue",type='l')
+legend ("topright",pch="", lty=1, col=c("black","red","blue"), legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+dev.off()
+
+
+
